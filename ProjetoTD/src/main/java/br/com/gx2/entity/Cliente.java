24931dd1df6e807489b2149bb8f8749c922a0581
tom.rodrigues <tom.rodrigues@gx2-129.gx2.local>
2020-02-12 15:36:13 -0200

    Commit

--- ProjetoTD/src/main/java/br/com/gx2/entity/Cliente.java
+++ ProjetoTD/src/main/java/br/com/gx2/entity/Cliente.java
@@ -1,5 +1,21 @@
 package br.com.gx2.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 @Entity
+@Table (name = "Cliente")
 public class Cliente {
 
+	@Id
+	@Column (name = "codigoCliente")
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	private Long codigoCliente;
+	
+	
+	
 }
