0e77e285eb781562904cff0699deade2b372b69c
tom.rodrigues <tom.rodrigues@gx2-129.gx2.local>
2020-02-12 17:00:15 -0200

    Commit

--- ProjetoTD/src/main/java/br/com/gx2/entities/Cliente.java
+++ ProjetoTD/src/main/java/br/com/gx2/entities/Cliente.java
@@ -1,15 +1,17 @@
 package br.com.gx2.entities;
 
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.Email;
 
 @Entity
 @Table(name = "Cliente")
-@ 
+@Data
 public class Cliente {
 
 	@Id
@@ -19,7 +21,8 @@ public class Cliente {
 
 	@Column(name = "nomeCliente")
 	private String nomeCliente;
-
+	
+	@Email (message = "VEI tu errou")
 	@Column(name = "emailCliente")
 	private String emailCliente;
 
--- ProjetoTD/src/main/java/br/com/gx2/entities/Curso.java
+++ ProjetoTD/src/main/java/br/com/gx2/entities/Curso.java
@@ -1,12 +1,16 @@
 package br.com.gx2.entities;
 
+
+import lombok.Data;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
 @Entity
 @Table (name = "Cursos")
+@Data
 public class Curso {
 	
-	
+	@Id
+	@Column (name = "codigoCursos")
 	
 }
